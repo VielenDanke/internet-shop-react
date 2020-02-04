08db55ca2d38640530f89e50640a3517df6fb846
Vladislav_Dankevich <Vladislav_Dankevich@epam.com>
2020-02-04 11:03:20 +0600

    Authorization module

--- src/Header.js
+++ src/Header.js
@@ -1,25 +1,97 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+    Route,
+    Switch
+} from 'react-router-dom';
+import AppHeader from './common/AppHeader';
+import Login from './user/login/Login';
+import Signup from './user/signup/Signup';
+import Profile from './user/profile/Profile';
+import OAuth2RedirectHandler from './user/oauth2/OAuth2RedirectHandler';
+import NotFound from './common/NotFound';
+import LoadingIndicator from './common/LoadingIndicator';
+import { getCurrentUser } from './util/APIUtils';
+import { ACCESS_TOKEN } from './constants';
+import PrivateRoute from './common/PrivateRoute';
+import Alert from 'react-s-alert';
+import 'react-s-alert/dist/s-alert-default.css';
+import 'react-s-alert/dist/s-alert-css-effects/slide.css';
+import './App.css';
+import Categories from "./goods/menu/GoodsCategories";
 
 class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            authenticated: false,
+            currentUser: null,
+            loading: false
+        }
+
+        this.loadCurrentlyLoggedInUser = this.loadCurrentlyLoggedInUser.bind(this);
+        this.handleLogout = this.handleLogout.bind(this);
+    }
+
+    loadCurrentlyLoggedInUser() {
+        this.setState({
+            loading: true
+        });
+
+        getCurrentUser()
+            .then(response => {
+                this.setState({
+                    currentUser: response,
+                    authenticated: true,
+                    loading: false
+                });
+            }).catch(error => {
+            this.setState({
+                loading: false
+            });
+        });
+    }
+
+    handleLogout() {
+        localStorage.removeItem(ACCESS_TOKEN);
+        this.setState({
+            authenticated: false,
+            currentUser: null
+        });
+        Alert.success("You're safely logged out!");
+    }
+
+    componentDidMount() {
+        this.loadCurrentlyLoggedInUser();
+    }
+
     render() {
+        if(this.state.loading) {
+            return <LoadingIndicator />
+        }
+
         return (
-            <div className="box-table">
-                <div className="box width80 box-col"></div>
-                <div className="box width20 box-col">
-                    {(this.props.authenticated) ? (
-                        <div>
-                            <a href="/office">office</a>
-                            <a href="/logout">logout</a>
-                        </div>
-                    ) : (
-                        <div>
-                            <a href="/login">login</a>
-                            <a href="/sign">sign up</a>
-                        </div>
-                    )}
+            <div className="app">
+                <div className="app-top-box">
+                    <AppHeader authenticated={this.state.authenticated} onLogout={this.handleLogout} />
                 </div>
+                <div className="app-body">
+                    <Switch>
+                        {/*<Route exact path="/" component={Categories}></Route>*/}
+                        <PrivateRoute path="/profile" authenticated={this.state.authenticated} currentUser={this.state.currentUser}
+                                      component={Profile}></PrivateRoute>
+                        <Route path="/login"
+                               render={(props) => <Login authenticated={this.state.authenticated} {...props} />}></Route>
+                        <Route path="/signup"
+                               render={(props) => <Signup authenticated={this.state.authenticated} {...props} />}></Route>
+                        <Route path="/oauth2/redirect" component={OAuth2RedirectHandler}></Route>
+                        {/*<Route component={NotFound}></Route>*/}
+                    </Switch>
+                </div>
+                <Alert stack={{limit: 3}}
+                       timeout = {3000}
+                       position='top-right' effect='slide' offset={65} />
             </div>
-        )
+        );
     }
 }
 
---
+++ src/user/profile/Profile.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './Profile.css';
+import {Link} from "react-router-dom";
+
+class Profile extends Component {
+    constructor(props) {
+        super(props);
+        console.log(props);
+    }
+
+    render() {
+        return (
+            <div className="profile-container">
+                <div className="container">
+                    <div className="profile-info">
+                        <div className="profile-avatar">
+                            {
+                                this.props.currentUser.picture ? (
+                                    <img src={this.props.currentUser.picture} alt={this.props.currentUser.fullName}/>
+                                ) : (
+                                    <div className="text-avatar">
+                                        <span>{this.props.currentUser.fullName && this.props.currentUser.fullName[0]}</span>
+                                    </div>
+                                )
+                            }
+                        </div>
+                        <div className="profile-name">
+                            <h2>{this.props.currentUser.fullName}</h2>
+                            <p className="profile-email">{this.props.currentUser.username}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Profile
